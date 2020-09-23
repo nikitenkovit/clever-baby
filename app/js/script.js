83c6f935d9c95f03310ddff45ee4868fb3129af2
Vitaly <65955977+nikitenkovit@users.noreply.github.com>
2020-09-23 13:35:30 +0300

    add header style tablet version

---
+++ app/js/script.js
@@ -0,0 +1,37 @@
+(function () {
+
+  /*show and hidden header navigation*/
+
+  const headerButton = document.querySelector('.header__button');
+  const navigationList = document.querySelector('.navigation__list');
+  const headerContact = document.querySelector('.header__contact');
+
+  const toggleHeaderButton = () => headerButton.classList.toggle('header__button--open');
+
+  const showOrHiddenNavigation = () => navigationList.classList.toggle('navigation__list--open');
+
+  const moveDownHeaderContact = () => headerContact.classList.toggle('header__contact--open');
+
+  headerButton.addEventListener('click', () => {
+    toggleHeaderButton();
+    showOrHiddenNavigation();
+    moveDownHeaderContact();
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})()
+
